ca7da4d4c7adef6bf6c9f03cf90f375e2644d2b3
Stygg <48440436+VIA-Jesper@users.noreply.github.com>
2024-03-13 17:12:02 +0100

    Add support files
    Add support files that is used to test the solution efficiency and alternative batching strategy.

---
+++ Models/BatchingStrategy/SizeBasedBatchingStrategy.cs
@@ -0,0 +1,11 @@
+﻿namespace DistinctWebAPI.Models.BatchingStrategy;
+
+public class SizeBasedBatchingStrategy(int batchSize) : IBatchingStrategy
+{
+    public IEnumerable<List<string>> GetBatches(IEnumerable<string> records)
+    {
+        return records.Select((value, index) => new { Value = value, Index = index })
+            .GroupBy(x => x.Index / batchSize)
+            .Select(group => group.Select(x => x.Value).ToList());
+    }
+}
---
+++ Resources/RandomWordGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace DistinctWebAPI.Utils;
+
+public class RandomWordGenerator
+{
+    private static Random random = new Random();
+    private static string[] punctuationMarks = { ".", ",", "!", "?", ";", ":" };
+
+    public static string GenerateRandomWordsInKB(int sizeInKB, int batchSize)
+    {
+        long totalBytes = sizeInKB * 1024L;
+        StringBuilder sb = new StringBuilder();
+
+        while (Encoding.UTF8.GetByteCount(sb.ToString()) < totalBytes)
+        {
+            sb.Append(GenerateBatch(batchSize));
+        }
+
+        return sb.ToString().Trim();
+    }
+
+    private static string GenerateBatch(int batchSize)
+    {
+        StringBuilder batchBuilder = new StringBuilder();
+
+        for (int i = 0; i < batchSize; i++)
+        {
+            string word = GenerateRandomWord(random.Next(3, 10)); // Generate random word with length between 3 to 10 characters
+            bool addPunctuation = random.Next(4) == 0; // 1 in 4 chance of adding punctuation
+            if (addPunctuation)
+            {
+                word += punctuationMarks[random.Next(punctuationMarks.Length)];
+            }
+            batchBuilder.Append(word);
+            batchBuilder.Append(" ");
+        }
+
+        return batchBuilder.ToString();
+    }
+
+    private static string GenerateRandomWord(int length)
+    {
+        const string chars = "abcdefghijklmnopqrstuvwxyz";
+        char[] word = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            word[i] = chars[random.Next(chars.Length)];
+        }
+        return new string(word);
+    }
+
+    public static string GenerateRandomWordsInMB(int sizeInMB)
+    {
+        return GenerateRandomWordsInKB(sizeInMB * 1024, 1024);
+    }
+
+}
